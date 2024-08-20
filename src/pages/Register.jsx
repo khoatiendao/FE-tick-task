e0f9159462eb8a6a5fb7e1548db08abbce31c490
khoatiendao <88887127+khoatiendao@users.noreply.github.com>
2024-08-20 19:16:17 +0700

    Fix template for captcha done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -173,6 +173,7 @@ const Register = () => {
                       }
                       className="w-28 rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-9 pl-2"
                     >
+                      <option hidden>Gender</option>
                       <option>Female</option>
                       <option>Male</option>
                       <option>Others</option>
--- src/pages/vertification/Captcha.jsx
+++ src/pages/vertification/Captcha.jsx
@@ -9,7 +9,6 @@ const Captcha = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { tokenEmail } = useParams();
-  const sitekeyCaptcha = process.env.REACT_APP_SITE_KEY_CAPTCHA;
 
   const handleCaptchaChange = (token) => {
     setCaptchaToken(token);
@@ -31,28 +30,28 @@ const Captcha = () => {
           body: JSON.stringify({ captchaToken: captchaToken }),
         }
       );
-
+      
       const result = await response.json();
-
+      
       if (result.success) {
         alert("Verification successful. Redirecting to login...");
-        // navigate("/login");
+        navigate("/login");
       } else {
         setVertificationError("Vertification failed. Please try again");
       }
     } catch (error) {
+      console.log(error);
       setVertificationError("An error occurred. Please try again.");
     }
   };
 
   return (
-    <div>
-      <h1>Email Verification</h1>
-      <ReCAPTCHA
-        sitekey="6LfFNykqAAAAAErIyMWr7nYGbfkLD94Fa0NAwfNM"
-        onChange={handleCaptchaChange}
-      />
-      <button onClick={handleVerify}>Verify Account</button>
+    <div className="flex items-center justify-center h-[80vh] flex-col">
+        <ReCAPTCHA
+          sitekey={process.env.REACT_APP_SITE_KEY_CAPTCHA}
+          onChange={handleCaptchaChange}
+        />
+        <button className="mt-8 px-4 py-2 bg-midnight text-white p-2 rounded-2xl" onClick={handleVerify}>Verify Account</button>
     </div>
   );
 };
